1e97a9e994f08dec83109828a83ab7321cc9a2b4
Swamim Saikia <31897267+Swamim18@users.noreply.github.com>
2019-03-28 14:43:27 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+a=input()
+consonants=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+if a in consonants:
+  print("Alphabet")
+else:
+  print("No")
