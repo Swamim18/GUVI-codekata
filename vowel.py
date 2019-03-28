d397d8a1c1b0ec48a3c6fe9ac15a271f4f5958a5
Swamim Saikia <31897267+Swamim18@users.noreply.github.com>
2019-03-28 14:39:50 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+a=input()
+vowels=["a","e","i","o","u"]
+consonants=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if a in vowels:
+  print("Vowel")
+elif a in consonants:
+  print("Consonant")
+else:
+  print("invalid")
